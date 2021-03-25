019cfedf15cefc8b0add3b0b905e3355de94b845
Olia <good-friday@mail.ru>
2021-03-26 01:15:30 +0300

    Исправление путей к изображениям

--- source/js/map.js
+++ source/js/map.js
@@ -26,13 +26,13 @@ L.tileLayer(
 ).addTo(map);
 
 const mainMarkerIcon = L.icon({
-  iconUrl: '../img/main-pin.svg',
+  iconUrl: 'img/main-pin.svg',
   iconSize: [52, 52],
   iconAnchor: [26, 52],
 });
 
 const markerIcon = L.icon({
-  iconUrl: '../img/pin.svg',
+  iconUrl: 'img/pin.svg',
   iconSize: [40, 40],
   iconAnchor: [20, 40],
 });
